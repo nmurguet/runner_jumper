fc5357129e8509eef143dfe9dca3fe3a39bed19b
Nicolas <nefisto@gmail.com>
2017-09-15 15:40:12 -0300

    commit inicial

---
+++ runner_jumper/Assets/scripts/CameraFollow.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Camera myCamera; 
+
+	public Transform target;
+
+	public float speed; 
+
+	public bool following; 
+	public float offset_x;
+	public float offset_y; 
+
+
+	public float zoom; 
+
+
+
+	private float store_zoom; 
+	public bool zoomout; 
+	// Use this for initialization
+	void Start () {
+		following = true; 
+		zoomout = false; 
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		zoom = Input.GetAxis ("Mouse ScrollWheel");
+		if (zoom > 0f) {
+			myCamera.orthographicSize -= 1f; 
+
+		}
+		if (zoom < 0f) {
+			myCamera.orthographicSize += 1f; 
+
+		}
+
+
+		if (following == true) {
+			if (target) {
+				transform.position = Vector3.Lerp (transform.position, target.position, speed) + new Vector3 (offset_x, offset_y, -10f);
+
+
+			}
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			if (!zoomout) {
+				store_zoom = myCamera.orthographicSize; 
+
+				myCamera.orthographicSize = 500f;
+				zoomout = true; 
+			} else {
+				myCamera.orthographicSize = store_zoom;
+				zoomout = false; 
+
+			}
+
+
+
+
+
+
+
+		}
+
+
+
+	}
+}
---
+++ runner_jumper/Assets/scripts/player_manager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_manager : MonoBehaviour {
+
+	private Rigidbody2D rb; 
+
+	private Animator anim; 
+
+
+	public float moveSpeed; 
+
+	public float jumpForce; 
+
+	private float store_grav; 
+	private float jump_grav = 0.1f;
+
+	//groundeck stuff
+
+	public Transform groundCheckPoint; 
+	public bool isGrounded; 
+	public float groundCheckRadius; 
+	public LayerMask whatIsGround; 
+
+
+
+	//Timer
+
+	public float timer; 
+	private float store_timer; 
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody2D> (); 
+		anim = GetComponent<Animator> (); 
+
+		store_grav = rb.gravityScale; 
+
+		store_timer = timer; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ManageAnimation ();
+		Movement (); 
+		
+	}
+
+
+	void ManageAnimation()
+	{
+		//manejo de animacion
+		anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+		anim.SetBool ("Grounded", isGrounded);
+
+
+	}
+
+	void FixedUpdate()
+	{
+		isGrounded = Physics2D.OverlapCircle (groundCheckPoint.position, groundCheckRadius, whatIsGround);
+
+	}
+
+
+	void Movement()
+	{
+		rb.velocity = new Vector2 (moveSpeed, rb.velocity.y);
+
+		if (Input.GetKey (KeyCode.Space)&&isGrounded) {
+				timer -= Time.deltaTime;
+				if (timer > 0) {
+					rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
+					rb.gravityScale = jump_grav;
+				} else {
+					rb.gravityScale = store_grav; 
+
+			
+				}
+			}
+
+			if (Input.GetKeyUp (KeyCode.Space)) {
+				rb.gravityScale = store_grav; 
+				timer = store_timer;
+			}
+
+		}
+
+
+}
